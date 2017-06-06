6f337d0c0b0bc9c19bd6ad6908d03fece8ea1dd4
ChargerIIC <ChargerIIC@Yahoo.com>
2017-06-06 14:55:32 -0700

    Initial Work

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,10 +5,18 @@ import { NavController } from 'ionic-angular';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage 
+{
+
+  private avatars = [
+    { name: 'BoomCat', imgPath: '../../assets/img/avatar/cat/Walk (1).png'}
+  ]
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  selectAvatar(avatar){
+    console.log(avatar.name + ' selected.');
+  }
 }
