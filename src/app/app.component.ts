eec058a6ee947b49a55cc7279234d5002812f269
ChargerIIC <ChargerIIC@Yahoo.com>
2017-06-23 10:56:35 -0700

    Removal of dice page
    The canvas pattern proved to be restrictive with device responsiveness.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { DicePageComponent } from '../pages/dice-page/dice-page';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +21,6 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'Dice', component: DicePageComponent }
     ];
 
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { Vibration } from '@ionic-native/vibration';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { DicePageComponent } from '../pages/dice-page/dice-page';
 import { DiceRollerPageComponent } from '../pages/dice-roller-page/dice-roller-page';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    DicePageComponent,
     DiceRollerPageComponent
   ],
   imports: [
@@ -25,12 +24,12 @@ import { DiceRollerPageComponent } from '../pages/dice-roller-page/dice-roller-p
   entryComponents: [
     MyApp,
     HomePage,
-    DicePageComponent,
     DiceRollerPageComponent
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    Vibration,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
